d2fed8297c2e94edd2c2d73212dd15af6ccc1cb9
Adriana Galvez <vntgh@yahoo.com>
2016-11-02 10:17:12 -0600

    retrieve score
    Please enter the commit message for your changes. Lines starting

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,18 @@ var q = require('../q/queries.js');
 
 router.post('/update', function(req, res, next) {
   q.updateRating(req.body).then(function(data){
-    res.send("done")
+    res.send("Updated!")
   })
 });
 
 router.get('/', function(req, res, next){
-  res.send("hello")
+  res.send("Up and Running.")
+})
+
+router.post('/gamers', function(req, res, next){
+  q.getRating(req.body.tag).then(function(rating){
+    res.json(rating[0].rating)
+  })
 })
 
 module.exports = router;
